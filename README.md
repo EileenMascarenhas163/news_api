74b5329184d7d981bc3cf2277077386f5376791e
Eileen Mascarenhas <a208.eileenmas@gmail.com>
2024-10-01 23:02:15 +0700

    sf\d

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ pip install -r requirements.txt
 
 
 #for running the file
+# linux
 uvicorn main:app --reload --port 8001
+# windows 
+python -m uvicorn main:app --reload --port 8001
+
--- main.py
+++ main.py
@@ -5,28 +5,41 @@ from fastapi import FastAPI
 app = FastAPI()
 
 def scrape_google_news_technology():
-    url = "https://news.google.com/topics/CAAqBwgKMAp7BwiwwMDqAw?hl=en-US&gl=US&ceid=US:en"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    articles = []
-    for item in soup.find_all('article'):
-        headline = item.find('h3')
-        if headline:
-            link = headline.find('a')['href']
-            full_link = f"https://news.google.com{link[1:]}"
-            articles.append({
-                "title": headline.get_text(),
-                "link": full_link
-            })
+    # URL of the technology section of Google News
+    url = "https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGRqTVhZU0JXVnVMVWRDR2dKSlRpZ0FQAQ?hl=en-IN&gl=IN&ceid=IN%3Aen"
     
-    return articles
-
-@app.get("/news/technology")
-def get_technology_news():
-    articles = scrape_google_news_technology()
+    # Sending a GET request to the URL
+    response = requests.get(url)
+    
+    # Checking if the request was successful
+    if response.status_code != 200:
+        print(f"Failed to retrieve news, status code: {response.status_code}")
+        return []
+    
+    # Parsing the content of the page using BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # List to hold the extracted articles
+    articles = []
+    
+    # Loop through all article links in the technology section
+    for item in soup.find_all('a', attrs={'class': 'DY5T1d'}):
+        # Extracting the title and the link
+        title = item.get_text()
+        link = item['href']
+        
+        # The link is relative, so we need to append the base URL
+        full_link = f"https://news.google.com{link[1:]}"
+        
+        # Adding the title and link to the articles list
+        articles.append({"title": title, "link": full_link})
+    
     return articles
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    # Calling the scrape function to get the articles
+    articles = scrape_google_news_technology()
+    
+    # Return the articles as JSON response
+    return {"articles": articles}
